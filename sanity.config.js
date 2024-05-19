18a9623f73e157840ed571a4ec9555bf880f811c
Tejas Gadi <tejasgadi@Tejass-MacBook-Pro.local>
2024-05-20 03:35:00 +0530

    Environment variables names modified

--- sanity.config.js
+++ sanity.config.js
@@ -7,8 +7,8 @@ import { schemaTypes } from "./schemas";
 export default defineConfig({
   name: "portfolio",
   title: "Portfolio Website",
-  projectId: process.env.SANITY_API_PROJECT_ID,
-  dataset: process.env.SANITY_API_DATASET,
+  projectId: process.env.NEXT_PUBLIC_SANITY_API_PROJECT_ID,
+  dataset: process.env.NEXT_PUBLIC_SANITY_API_DATASET,
   basePath: "/studio",
   plugins: [deskTool()],
   schema: { types: schemaTypes },
